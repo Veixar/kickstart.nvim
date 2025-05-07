6d116bab0244f25ca72c3351eff7cc9843952ac8
Veixar <veixar@gmail.com>
2025-05-07 18:00:00 +0200

    Add Tmux key bindings to split windows.

--- init.lua
+++ init.lua
@@ -107,10 +107,16 @@ vim.keymap.set('n', '<leader>h', '<C-w><C-h>', { desc = 'Move focus to the left 
 vim.keymap.set('n', '<leader>l', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<leader>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Same format as above to switch to the last active window
 vim.keymap.set('n', '<leader>w', '<C-w><C-w>', { desc = 'Switch windows' })
 
+-- Use the same key bindings to split windows as Tmux
+vim.keymap.set('n', '<leader>%', '<C-w><C-v>', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>"', '<C-w><C-s>', { desc = 'Split window horizontally' })
+
 -- Open default file explorer when pressing '-' in normal mode
-vim.keymap.set('n', '-', '<cmd>Explore<CR>')
+vim.keymap.set('n', '-', '<cmd>Explore<CR>', { desc = 'Open file explorer' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
