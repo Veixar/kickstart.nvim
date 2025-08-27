d45f91f16c869f408ab1cde8d665a7fe93308fc5
Veixar <veixar@gmail.com>
2025-08-27 10:14:13 +0200

    Add venv-selector.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,23 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+      },
+    },
+    ft = 'python', -- Load when opening Python files
+    keys = {
+      { ',v', '<cmd>VenvSelect<cr>' }, -- Open picker on keymap
+    },
+    opts = {
+      -- Your settings go here
+    },
+  },
+}
